be9e393f605fcd7cfaab202c61151070442dcf97
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2022-09-14 13:32:16 +0300

    add module 07 BEM

--- js/animate.js
+++
@@ -1,8 +0,0 @@
-const element = document.querySelector(".hero-btn");
-
-element.addEventListener("click", () => {
-  element.classList.add("animate__rubberBand");
-  setTimeout(() => {
-    element.classList.remove("animate__rubberBand");
-  }, 1000);
-});
